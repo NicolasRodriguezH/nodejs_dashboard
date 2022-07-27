e3aff6e37906cd68fb9c9742e3c2b79b46e3ed4c
Nicolas <nicolashilarion420@gmail.com>
2022-07-27 13:26:07 -0500

    dashboard in nodejs is running

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Utilizamos el router
+const router = require('./routes/router')
+app.use(router.routes)
+
+
+/* app.get('/', (req, res)=> {
+    res.send('Dashboard con Nodejs')
+}) */
+
+app.listen(3000, () => {
+     console.log('Server is running in http://localhost:3000')
+})
